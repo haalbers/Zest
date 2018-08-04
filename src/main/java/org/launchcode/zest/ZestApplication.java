632c986703edb580b9fcd945469b2a0f9650e9c9
Amanda Owens <amanda7641@gmail.com>
2018-08-04 11:10:41 -0500

    Creating project

---
+++ src/main/java/org/launchcode/zest/ZestApplication.java
@@ -0,0 +1,12 @@
+package org.launchcode.zest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZestApplication.class, args);
+	}
+}
---
+++ src/test/java/org/launchcode/zest/ZestApplicationTests.java
@@ -0,0 +1,16 @@
+package org.launchcode.zest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
